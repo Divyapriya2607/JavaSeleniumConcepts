2eb1759e25b041649bbb827bf62925a34900bd7a
Divyapriya207 <priyadivya51@gmail.com>
2024-10-06 14:23:14 +0530

    added the validation file

--- SELENIUMJAVAPRACTICE/src/main/java/SELENIUMCONCEPTS/ScrollDownExampleUsingJavaScript.java
+++ SELENIUMJAVAPRACTICE/src/main/java/SELENIUMCONCEPTS/ScrollDownExampleUsingJavaScript.java
@@ -1,16 +1,22 @@
 package SELENIUMCONCEPTS;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ScrollDownExampleUsingJavaScript {
-
+	static WebDriver driver;
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options=new ChromeOptions();
 		options.addArguments("start-maximized");
-		
+		driver=new ChromeDriver(options);
+		driver.get("https://www.amazon.com");
+		JavascriptExecutor jse=(JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,1000)");
 
 	}
 
---
+++ SELENIUMJAVAPRACTICE/src/main/java/SELENIUMCONCEPTS/ToValidateTitleOfThePage.java
@@ -0,0 +1,33 @@
+package SELENIUMCONCEPTS;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import dev.failsafe.internal.util.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ToValidateTitleOfThePage {
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://amazon.in");
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Online Shopping site in India";
+		if (actualTitle.equalsIgnoreCase(expectedTitle)) {
+			System.out.println("Title Matched");
+		} else {
+			System.out.println("Title not matched");
+		}
+		//Assert.assertEquals(actualTitle,expectedTitle);
+
+	}
+
+}
